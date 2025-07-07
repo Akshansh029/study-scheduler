64256f5deeb33a06e0bfeaafc36a37d885a10878
Akshansh <akshanshsingh00@gmail.com>
2025-07-08 02:27:39 +0530

    Added cron job API route to public routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ const isPublicRoute = createRouteMatcher([
   "/sign-in(.*)",
   "/sign-up(.*)",
   "/",
-  // "/api/cron/reset-sessions",
+  "/api/cron/reset-sessions",
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
